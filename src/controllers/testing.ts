72c340dc97e529479ff5a66e07791144f0fc422f
OwnDesire <glorytoheroes154@gmail.com>
2024-03-17 18:33:15 +0200

    Exercise 5.22 result: replaced deprecated api.

--- src/controllers/testing.ts
+++ src/controllers/testing.ts
@@ -8,7 +8,7 @@ testingRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({});
   await User.deleteMany({});
 
-  response.send(204).end();
+  response.status(204).end();
 });
 
 export default testingRouter;
